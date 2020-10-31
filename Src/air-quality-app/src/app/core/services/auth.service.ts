90e2779f831926e8ee8a4f5a030f80dcd76bcb94
Ismar Mujagic <ismar.mujagic@gmail.com>
2020-10-31 17:19:27 +0100

    move services to core/services folder

--- Src/air-quality-app/src/app/core/services/auth.service.ts
+++ Src/air-quality-app/src/app/core/services/auth.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { LoginModel } from '../models/login.model';
+import { RegisterModel } from '../models/register.model';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,5 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
 }
