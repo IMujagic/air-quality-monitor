1b3e025e87ab34dba549c08824a904bf8c2036b3
Ismar Mujagic <ismar.mujagic@gmail.com>
2020-10-31 19:59:44 +0100

    add infinite scroll component

--- Src/air-quality-app/src/app/core/services/air-quality.service.ts
+++ Src/air-quality-app/src/app/core/services/air-quality.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AirQualityService {
+  // TODO: move to config
+  base_path = 'http://localhost:3000/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  fetch(page: number) {
+    return this.http.get(this.base_path + 'measurements?_page='+page+'&_limit=20');
+  }
+
 }
--- Src/air-quality-app/src/app/home/home.page.ts
+++ Src/air-quality-app/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
+import { AirQualityService } from '../core/services/air-quality.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
+  
+  url: string;
+  measurements = [];
+  page = 1;
+  constructor(private airQualityService: AirQualityService) {}
 
-  constructor() {}
+  ngOnInit() {
+    this.loadMore({}, true);
+  }
 
+  loadMore(event, initialLoad: boolean) {
+    this.airQualityService.fetch(this.page)
+      .subscribe((data: any[]) => {
+
+        this.measurements = this.measurements.concat(data);
+        this.page++;
+
+        if(!initialLoad) {
+          event.target.complete();
+        }
+      }, error => {
+        console.log(error);
+      })
+  }
 }
