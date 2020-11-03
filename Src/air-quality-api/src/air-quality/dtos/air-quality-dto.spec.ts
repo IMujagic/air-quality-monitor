69885140f75c4d813db41892b368762c63bf0891
Ismar Mujagic <ismar.mujagic@gmail.com>
2020-11-03 23:35:05 +0100

    remove dto spec files

--- Src/air-quality-api/src/air-quality/dtos/air-quality-dto.spec.ts
+++
@@ -1,7 +0,0 @@
-import { AirQualityDto } from './air-quality-dto';
-
-describe('AirQualityDto', () => {
-  it('should be defined', () => {
-    expect(new AirQualityDto()).toBeDefined();
-  });
-});
