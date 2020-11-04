20466d0538f6c8ad5c9b55154bd8134150046fb2
Ismar Mujagic <ismar.mujagic@gmail.com>
2020-11-04 01:44:57 +0100

    add dockerfile for ionic app and enable env

--- Src/air-quality-app/src/environments/environment.prod.ts
+++ Src/air-quality-app/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: false,
+  apiUrl: window["env"]["apiUrl"] || "default",
+  storageKey: window["env"]["storageKey"] || false
 };
--- Src/air-quality-app/src/environments/environment.ts
+++ Src/air-quality-app/src/environments/environment.ts
@@ -5,7 +5,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000',
-  pageSize: 50,
   storageKey: 'user' 
 };
 
